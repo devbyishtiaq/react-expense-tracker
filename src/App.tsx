26c624aa9daf1cb07777b83b132737b8b08cc12d
devbyishtiaq <ishtiaaq.mushtaaq@gmail.com>
2023-10-01 15:25:05 +0500

    added expense list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
+const categories = [
+  { id: 1, name: "All Categories" },
+  { id: 2, name: "Utilities" },
+  { id: 3, name: "Groceries" },
+  { id: 4, name: "Entertainment" },
+];
+
 function App() {
-  <div className="">Hello World!</div>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "hello world", amount: 100, category: "Utilities" },
+    { id: 2, description: "hello world", amount: 100, category: "Utilities" },
+    { id: 3, description: "hello world", amount: 100, category: "Utilities" },
+  ]);
+
+  return (
+    <div className="container mx-auto min-h-screen overflow-hidden p-8">
+      <h1 className="text-3xl font-bold tracking-wider text-center">
+        Expense Tracker
+      </h1>
+      <div className="grid grid-cols-2 gap-8 mt-16">
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
